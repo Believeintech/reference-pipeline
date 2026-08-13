3102b11cc42546fd2b7c005c05aff55a66aa6065
Believeintech <rajeshrahul984@gmail.com>
2026-08-13 08:34:29 +0530

    Use isolated clone of GitOps-Continuous-Deployment-Platform repo instead of in-place edit

--- vars/updateManifestInGit.groovy
+++ vars/updateManifestInGit.groovy
@@ -1,14 +1,17 @@
 def call(String dockerHubUser, String serviceName, String buildTag, String manifestPath, String gitCredentialsId) {
     withCredentials([usernamePassword(credentialsId: gitCredentialsId, usernameVariable: 'GIT_USER', passwordVariable: 'GIT_TOKEN')]) {
         sh """
+          rm -rf gitops-clone-${serviceName}-${buildTag}
+          git clone https://\$GIT_USER:\$GIT_TOKEN@github.com/Believeintech/GitOps-Continuous-Deployment-Platform.git gitops-clone-${serviceName}-${buildTag}
+          cd gitops-clone-${serviceName}-${buildTag}
           sed -i "s|image:.*|image: ${dockerHubUser}/${serviceName}:${buildTag}|" ${manifestPath}
           git config user.email "jenkins@ci.local"
           git config user.name "Jenkins CI"
           git add ${manifestPath}
           git commit -m "Update ${serviceName} image to ${buildTag} [skip ci]"
-          git fetch https://\$GIT_USER:\$GIT_TOKEN@github.com/Believeintech/microservices-demo.git main
-          git rebase FETCH_HEAD
-          git push https://\$GIT_USER:\$GIT_TOKEN@github.com/Believeintech/microservices-demo.git HEAD:main
+          git push origin main
+          cd ..
+          rm -rf gitops-clone-${serviceName}-${buildTag}
         """
     }
 }
